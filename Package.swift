45ab83c0aa46e7f4e1571162863473445d3deeeb
Burak Kebapci <burak@socure.com>
2023-02-02 20:18:58 -0500

    new SPM

--- Package.swift
+++ Package.swift
@@ -10,25 +10,17 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "SigmaDevice",
-            targets: ["SigmaDeviceWrapper"]),
-        .library(
-            name: "SigmaDeviceWithoutTrustKit",
-            targets: ["DeviceRisk"])
+            targets: ["SigmaDeviceWrapper"])
     ],
     targets: [
         .binaryTarget(
             name: "DeviceRisk",
             path: "Framework/DeviceRisk.xcframework"
         ),
-        .binaryTarget(
-            name: "TrustKit",
-            path: "Framework/TrustKit.xcframework"
-        ),
         .target(
             name: "SigmaDeviceWrapper",
             dependencies: [
-                .target(name: "DeviceRisk"),
-                .target(name: "TrustKit")
+                .target(name: "DeviceRisk")
             ],
             path: "SigmaDeviceWrapper"
         ),
